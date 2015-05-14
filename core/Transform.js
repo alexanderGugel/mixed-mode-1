f441f6c1c00ca30a20f3879fc73069815ff697d7
DnMllr <me@dnmllr.io>
2015-05-14 16:17:04 -0700

    chore: lint

--- core/Transform.js
+++ core/Transform.js
@@ -24,18 +24,13 @@
 
 'use strict';
 
-var pathUtils = require('./Path');
-
-/**
- * The transform class is responsible for calculating the transform of a particular
- * node from the data on the node and its parent
- *
- * @constructor {Transform}
- */
 function Transform () {
-    this._matrix = new Float32Array(16);
-    this._transforms = [];
-    this._paths = [];
+    this.local = new Float32Array(Transform.IDENT);
+    this.global = new Float32Array(Transform.IDENT);
+    this.needsUpdate = false;
+    this.parent = null;
+    this.breakPoint = false;
+    this.world = false;
 }
 
 Transform.IDENT = [ 1, 0, 0, 0,
@@ -43,254 +38,18 @@ Transform.IDENT = [ 1, 0, 0, 0,
                     0, 0, 1, 0,
                     0, 0, 0, 1 ];
 
-Transform.DEFAULT_OPTIONS = {
-    transform: Transform.IDENT,
-    breakPoint: false,
-    needsWorld: false
+Transform.prototype.setParent = function setParent (parent) {
+    this.parent = parent;
 };
 
-Transform.prototype.registerTransformAtPath = function registerTransformAtPath (path, options) {
-    var index = this._paths.indexOf(path);
-    if (index === -1) {
-        options = options ? options : Transform.DEFAULT_OPTIONS;
-        var i = 0;
-        var len = this._paths.length;
-        var targetDepth = depth(path);
-        while (i < len && depth(this._paths[i]) < targetDepth) i++;
-        this._paths.splice(i, 0, path);
-        this._transforms.splice(i, 0, {
-            transform: new Float32Array(options.transform ? options.transform : Transform.IDENT),
-            parent: this._transforms[this._paths.indexOf(pathUtils.parent(path))],
-            breakPoint: options.breakPoint != null ? options.breakPoint : false,
-            needsWorld: options.needsWorld != null ? options.breakPoint : false
-        });
-    }
+Transform.prototype.getLocalTransform = function getLocalTransform () {
+    return this.local;
 };
 
-Transform.prototype.evaluate = function evaluate () {
-    var parent;
-    var target;
-    for (var i = 0, len = this._transforms.length ; i < len ; i++) {
-        target = this._transform[i];
-        parent = !target.parent ? Transform.DEFAULT_OPTIONS : target.parent;
-
-
-
-
-/**
- * Returns the last calculated transform
- *
- * @return {Array} a transform
- */
-Transform.prototype.get = function get () {
-    return this._matrix;
-};
-
-/**
- * Creates a transformation matrix from a Node's spec.
- *
- * @method fromSpec
- * 
- * @param {Node.Spec} spec of the node
- * @param {Array} size of the node
- * @param {Array} size of the node's parent
- * @param {Array} target array to write the matrix to
- * 
- * @return {Boolean} whether or not the target array was changed
- */
-Transform.prototype.fromSpec = function fromSpec (spec, mySize, parentSize, target) {
-    target = target ? target : this._matrix;
-    var changed = target ? false : true;
-
-    var t00         = target[0];
-    var t01         = target[1];
-    var t02         = target[2];
-    var t10         = target[4];
-    var t11         = target[5];
-    var t12         = target[6];
-    var t20         = target[8];
-    var t21         = target[9];
-    var t22         = target[10];
-    var t30         = target[12];
-    var t31         = target[13];
-    var t32         = target[14];
-    var posX        = spec.vectors.position[0];
-    var posY        = spec.vectors.position[1];
-    var posZ        = spec.vectors.position[2];
-    var rotX        = spec.vectors.rotation[0];
-    var rotY        = spec.vectors.rotation[1];
-    var rotZ        = spec.vectors.rotation[2];
-    var rotW        = spec.vectors.rotation[3];
-    var scaleX      = spec.vectors.scale[0];
-    var scaleY      = spec.vectors.scale[1];
-    var scaleZ      = spec.vectors.scale[2];
-    var alignX      = spec.offsets.align[0] * parentSize[0];
-    var alignY      = spec.offsets.align[1] * parentSize[1];
-    var alignZ      = spec.offsets.align[2] * parentSize[2];
-    var mountPointX = spec.offsets.mountPoint[0] * mySize[0];
-    var mountPointY = spec.offsets.mountPoint[1] * mySize[1];
-    var mountPointZ = spec.offsets.mountPoint[2] * mySize[2];
-    var originX     = spec.offsets.origin[0] * mySize[0];
-    var originY     = spec.offsets.origin[1] * mySize[1];
-    var originZ     = spec.offsets.origin[2] * mySize[2];
-
-    var wx = rotW * rotX;
-    var wy = rotW * rotY;
-    var wz = rotW * rotZ;
-    var xx = rotX * rotX;
-    var yy = rotY * rotY;
-    var zz = rotZ * rotZ;
-    var xy = rotX * rotY;
-    var xz = rotX * rotZ;
-    var yz = rotY * rotZ;
-
-    target[0] = (1 - 2 * (yy + zz)) * scaleX;
-    target[1] = (2 * (xy + wz)) * scaleX;
-    target[2] = (2 * (xz - wy)) * scaleX;
-    target[3] = 0;
-    target[4] = (2 * (xy - wz)) * scaleY;
-    target[5] = (1 - 2 * (xx + zz)) * scaleY;
-    target[6] = (2 * (yz + wx)) * scaleY;
-    target[7] = 0;
-    target[8] = (2 * (xz + wy)) * scaleZ;
-    target[9] = (2 * (yz - wx)) * scaleZ;
-    target[10] = (1 - 2 * (xx + yy)) * scaleZ;
-    target[11] = 0;
-    target[12] = alignX + posX - mountPointX + originX - (rs0 * originX + rs3 * originY + rs6 * originZ);
-    target[13] = alignY + posY - mountPointY + originY - (rs1 * originX + rs4 * originY + rs7 * originZ);
-    target[14] = alignZ + posZ - mountPointZ + originZ - (rs2 * originX + rs5 * originY + rs8 * originZ);
-    target[15] = 0;
-
-    return changed 
-        || t00 !== target[0]
-        || t01 !== target[1]
-        || t02 !== target[2]
-        || t10 !== target[4]
-        || t11 !== target[5]
-        || t12 !== target[6]
-        || t20 !== target[8]
-        || t21 !== target[9]
-        || t22 !== target[10]
-        || t30 !== target[12]
-        || t31 !== target[13]
-        || t32 !== target[14];
-};
-
-/**
- * Uses the parent transform, the node's spec, the node's size, and the parent's size
- * to calculate a final transform for the node. Returns true if the transform has changed.
- *
- * @param {Array} the parent matrix
- * @param {Node.Spec} the target node's spec
- * @param {Array} the size of the node
- * @param {Array} the size of the parent
- * @param {Array} the target array to write the resulting transform to
- *
- * @return {Boolean} whether or not the transform changed
- */
-Transform.prototype.fromSpecWithParent = function fromSpecWithParent (parentMatrix, spec, mySize, parentSize, target) {
-    target = target ? target : this._matrix;
-    var changed = target ? false : true;
-
-    // local cache of everything
-    var t00         = target[0];
-    var t01         = target[1];
-    var t02         = target[2];
-    var t10         = target[4];
-    var t11         = target[5];
-    var t12         = target[6];
-    var t20         = target[8];
-    var t21         = target[9];
-    var t22         = target[10];
-    var t30         = target[12];
-    var t31         = target[13];
-    var t32         = target[14];
-    var p00         = parentMatrix[0];
-    var p01         = parentMatrix[1];
-    var p02         = parentMatrix[2];
-    var p10         = parentMatrix[4];
-    var p11         = parentMatrix[5];
-    var p12         = parentMatrix[6];
-    var p20         = parentMatrix[8];
-    var p21         = parentMatrix[9];
-    var p22         = parentMatrix[10];
-    var p30         = parentMatrix[12];
-    var p31         = parentMatrix[13];
-    var p32         = parentMatrix[14];
-    var posX        = spec.vectors.position[0];
-    var posY        = spec.vectors.position[1];
-    var posZ        = spec.vectors.position[2];
-    var rotX        = spec.vectors.rotation[0];
-    var rotY        = spec.vectors.rotation[1];
-    var rotZ        = spec.vectors.rotation[2];
-    var rotW        = spec.vectors.rotation[3];
-    var scaleX      = spec.vectors.scale[0];
-    var scaleY      = spec.vectors.scale[1];
-    var scaleZ      = spec.vectors.scale[2];
-    var alignX      = spec.offsets.align[0] * parentSize[0];
-    var alignY      = spec.offsets.align[1] * parentSize[1];
-    var alignZ      = spec.offsets.align[2] * parentSize[2];
-    var mountPointX = spec.offsets.mountPoint[0] * mySize[0];
-    var mountPointY = spec.offsets.mountPoint[1] * mySize[1];
-    var mountPointZ = spec.offsets.mountPoint[2] * mySize[2];
-    var originX     = spec.offsets.origin[0] * mySize[0];
-    var originY     = spec.offsets.origin[1] * mySize[1];
-    var originZ     = spec.offsets.origin[2] * mySize[2];
-
-    var wx = rotW * rotX;
-    var wy = rotW * rotY;
-    var wz = rotW * rotZ;
-    var xx = rotX * rotX;
-    var yy = rotY * rotY;
-    var zz = rotZ * rotZ;
-    var xy = rotX * rotY;
-    var xz = rotX * rotZ;
-    var yz = rotY * rotZ;
-
-    var rs0 = (1 - 2 * (yy + zz)) * scaleX;
-    var rs1 = (2 * (xy + wz)) * scaleX;
-    var rs2 = (2 * (xz - wy)) * scaleX;
-    var rs3 = (2 * (xy - wz)) * scaleY;
-    var rs4 = (1 - 2 * (xx + zz)) * scaleY;
-    var rs5 = (2 * (yz + wx)) * scaleY;
-    var rs6 = (2 * (xz + wy)) * scaleZ;
-    var rs7 = (2 * (yz - wx)) * scaleZ;
-    var rs8 = (1 - 2 * (xx + yy)) * scaleZ;
-
-    var tx = alignX + posX - mountPointX + originX - (rs0 * originX + rs3 * originY + rs6 * originZ);
-    var ty = alignY + posY - mountPointY + originY - (rs1 * originX + rs4 * originY + rs7 * originZ);
-    var tz = alignZ + posZ - mountPointZ + originZ - (rs2 * originX + rs5 * originY + rs8 * originZ);
-
-    target[0] = p00 * rs0 + p10 * rs1 + p20 * rs2;
-    target[1] = p01 * rs0 + p11 * rs1 + p21 * rs2;
-    target[2] = p02 * rs0 + p12 * rs1 + p22 * rs2;
-    target[3] = 0;
-    target[4] = p00 * rs3 + p10 * rs4 + p20 * rs5;
-    target[5] = p01 * rs3 + p11 * rs4 + p21 * rs5;
-    target[6] = p02 * rs3 + p12 * rs4 + p22 * rs5;
-    target[7] = 0;
-    target[8] = p00 * rs6 + p10 * rs7 + p20 * rs8;
-    target[9] = p01 * rs6 + p11 * rs7 + p21 * rs8;
-    target[10] = p02 * rs6 + p12 * rs7 + p22 * rs8;
-    target[11] = 0;
-    target[12] = p00 * tx + p10 * ty + p20 * tz + p30;
-    target[13] = p01 * tx + p11 * ty + p21 * tz + p31;
-    target[14] = p02 * tx + p12 * ty + p22 * tz + p32;
-    target[15] = 1;
-
-    return changed 
-        || t00 !== target[0]
-        || t01 !== target[1]
-        || t02 !== target[2]
-        || t10 !== target[4]
-        || t11 !== target[5]
-        || t12 !== target[6]
-        || t20 !== target[8]
-        || t21 !== target[9]
-        || t22 !== target[10]
-        || t30 !== target[12]
-        || t31 !== target[13]
-        || t32 !== target[14];
+Transform.prototype.getGlobalTransform = function getGlobalTransform () {
+    if (!this.world) throw new Error('This transform is not calculating world transforms');
+    return this.global;
 };
 
 module.exports = Transform;
+
