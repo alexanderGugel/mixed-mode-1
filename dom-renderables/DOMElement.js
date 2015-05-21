8a0ae239e9c54a30e19f9c9286eae9797d641936
DnMllr <me@dnmllr.io>
2015-05-20 23:39:31 -0700

    fix: transform changes after rebase

--- dom-renderables/DOMElement.js
+++ dom-renderables/DOMElement.js
@@ -58,32 +58,27 @@ function DOMElement(node, options) {
 
     this._node = node;
     this._parent = null;
+    this._changeQueue = [];
     this._children = [];
 
     this._requestingUpdate = false;
     this._renderSized = false;
     this._requestRenderSize = false;
 
-    this._changeQueue = [];
-
     this._UIEvents = node.getUIEvents().slice(0);
     this._classes = ['famous-dom-element'];
     this._requestingEventListeners = [];
     this._styles = {};
 
-    this.setProperty('display', node.isShown() ? 'none' : 'block');
-    this.onOpacityChange(node.getOpacity());
-
     this._attributes = {};
     this._content = '';
 
     this._tagName = options && options.tagName ? options.tagName : 'div';
-    this._id = node ? node.addComponent(this) : null;
-
     this._renderSize = [0, 0, 0];
 
     this._callbacks = new CallbackStore();
 
+    this._id = node ? node.addComponent(this) : null;
     this.setProperty('display', node.isShown() ? 'none' : 'block');
     this.onOpacityChange(node.getOpacity());
 
